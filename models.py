08b62e9aab4e7f2ae12156cbdaef4d4a26639773
vanessa2493 <56240421+vanessa2493@users.noreply.github.com>
2023-08-09 21:07:27 -0300

    method createCatalogo created and working

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 # Create class Pelicula and CatalogoPelicula
 
 class Pelicula:
+
     def __init__(self, nombrePelicula):
         self.__nombrePelicula: nombrePelicula
 
@@ -16,11 +17,30 @@ class Pelicula:
         self.__nombrePelicula = nombrePelicula
 
 
-class CatalogoPelicula(Pelicula):
-    def __init__(self, nombreCatalogo, ruta_archivo, nombrePelicula):
-        super().__init__(nombrePelicula)
+class CatalogoPelicula:
+
+    def __init__(self, nombreCatalogo: str):
         self.nombreCatalogo = nombreCatalogo
-        self.ruta_archivo = ruta_archivo
+        self.ruta_archivo = f'{self.nombreCatalogo}.txt'
 
     def __str__(self):
         return f"nombreCatalogo:{self.nombreCatalogo}, ruta_archivo: {self.ruta_archivo}"
+
+    def crearCatalogo(self, nombreCatalogo: str):
+        with open(f'{nombreCatalogo}.txt', 'a+') as file:
+            file.write(f'{self.ruta_archivo}\n')
+
+    def agregarPelicula(self):
+
+        pass
+
+    def listarPeliculas(self):
+        pass
+
+    def eliminarCatalogo(self):
+        pass
+
+
+catalogoPrueba = CatalogoPelicula("catalogo1")
+
+catalogoPrueba.crearCatalogo("catalogo1")
