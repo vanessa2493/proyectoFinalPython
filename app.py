2c79085ab28a014e358495abed5dcddc970202f9
vanessa2493 <56240421+vanessa2493@users.noreply.github.com>
2023-08-13 12:26:15 -0300

    integration of the app and models

--- app.py
+++ app.py
@@ -1,31 +1,88 @@
 # Import models.py
 
-import models
+from models import CatalogoPelicula as cp
+from models import Pelicula
 
-# App
+# Import Libaries
 
-print("_______________________")
-print("Catálogo de Películas")
-print("_______________________")
+import os
+
+## App ##
+
+print("***********************")
+print(" Catálogo de Películas")
+print("***********************\n")
+
+# Initialize variables
 
 opcion = 0
+catalogo = None
 
-while opcion != 4:
+# Starting app menu
+
+while opcion != 6:
+    if catalogo == None:
+        print("______________________________________")
+        print('\nDebe crear o seleccionar un catálogo\n')
+    else:
+        print(f'El catálogo seleccionado es {catalogo.nombreCatalogo}\n')
+
     print("Seleccione una de las siguientes opciones\n",
-          "1: Agregar película\n",
-          "2: Listar películas\n",
-          "3: Eliminar catálogo películas\n",
-          "4: Salir\n")
+          "1: Agregar o seleccionar catálogo\n",
+          "2: Listar catálogos\n",
+          "3: Agregar película\n",
+          "4: Listar películas\n",
+          "5: Eliminar catálogo películas\n",
+          "6: Salir\n")
 
-    opcion = int(input('Ingrese una opción: '))
+    opcion = input('Ingrese una opción: ')
+    opcion = int(opcion)
 
     if (opcion == 1):
-        print("agregar pelicula\n")
+        print("Agregar o seleccionar catálogo\n")
+        catalogo = cp(input("Ingrese el nombre del catálogo: \n"))
+        cp.verificarExistenciaCatalogo(catalogo)
+
     elif (opcion == 2):
-        print("listar pelicula\n")
+        print("Los catálogos disponibles son: \n")
+        txt_files = [file for file in os.listdir(
+            "./") if os.path.splitext(file)[1] == ".txt"]
+        print(txt_files, '\n')
+
     elif (opcion == 3):
-        print("eliminar cat pelicula\n")
+        try:
+            print("Agregar pelicula\n")
+            cp.agregarPelicula(catalogo, Pelicula(input(
+                "Ingrese el nombre de la película: \n")))
+        except:
+            print("No ha seleccionado ningún catálogo")
+
     elif (opcion == 4):
-        print("salir\n")
+        try:
+            print("Listar peliculas\n")
+            cp.listarPeliculas(catalogo)
+        except:
+            print("No ha seleccionado ningún catálogo")
+
+    elif (opcion == 5):
+        try:
+            print("Eliminar catálogo películas\n")
+            print(
+                f"¿Está segurx que desea eliminar el catálogo: {catalogo.nombreCatalogo}? ")
+            opcion_eliminar = input("si/no\n")
+
+            if (opcion_eliminar == "si"):
+                cp.eliminarCatalogo(catalogo)
+                catalogo = None
+            elif (opcion_eliminar == "no"):
+                print("No ha eliminado ningún catálogo")
+            else:
+                print("Elija una opción correcta: si/no")
+        except:
+            print("No ha seleccionado ningún catálogo")
+
+    elif (opcion == 6):
+        print("Ha salido de la aplicación\n")
+
     else:
-        print("\nOpción incorrecta, elija una opción del 1 al 4.")
+        print("\nOpción incorrecta, elija una opción del 1 al 6.")
